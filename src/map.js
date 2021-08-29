6c2669cdaa87ab350e9d2a52f3561869f896fc5e
AnjithPaul <anjithpaul.mec@gmail.com>
2021-08-29 18:40:47 +0530

    Replace builtin function in map

--- src/map.js
+++ src/map.js
@@ -1,5 +1,16 @@
-const map = (first,second) => {
-    return first.map(second);;   
+const map = (arr,fun) => {
+    return mappingRec(arr, fun, 0, []);   
+}
+
+const mappingRec = (arr, fun, i, newArr) => {
+    element = arr[i];
+    
+    if(element == undefined)
+        return newArr
+    
+    newArr.push(fun(arr[i]));
+
+    return mappingRec(arr, fun, i+1, newArr);
 }
 
 const identity = (x) => {
