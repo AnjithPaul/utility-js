8924a7ba33e4038b52340fbaf15bfe36b2586a2a
AnjithPaul <anjithpaul.mec@gmail.com>
2021-08-29 18:45:18 +0530

    Replace builtin function in filter

--- src/filter.js
+++ src/filter.js
@@ -1,5 +1,17 @@
-const filter = (first,second) => {
-    return first.filter(second);;   
+const filter = (arr,fun) => {
+    return filteringRec(arr, fun, 0, []);
+}
+
+const filteringRec = (arr, fun, i, newArr) => {
+    element = arr[i];
+
+    if(element == undefined)
+        return newArr;
+    
+    if(fun(element))
+        newArr.push(element);
+    
+    return filteringRec(arr, fun, i+1, newArr);
 }
 
 const filterUpperCase = (x) => {
