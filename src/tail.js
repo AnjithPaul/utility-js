413de96702d828e5c4bf2205fd3650ca67c868cb
AnjithPaul <anjithpaul.mec@gmail.com>
2021-08-29 18:33:15 +0530

    Replace builtin function in tail

--- src/tail.js
+++ src/tail.js
@@ -1,5 +1,16 @@
 const tail = (list) => {
-    list.shift();   
-    return list;
+    return tailEnd(list, 1, []);
+}
+
+const tailEnd = (list, i, newList) => {
+
+    element = list[i];
+
+    if(element == undefined)
+        return newList;
+    
+    newList.push(element);
+
+    return tailEnd(list, i+1, newList)
 }
 module.exports = tail;
